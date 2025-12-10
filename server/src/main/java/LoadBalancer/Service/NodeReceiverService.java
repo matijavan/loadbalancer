d0e670dee9f988fa20047c794193502c71820c11
Matija <matija.vandek@gmail.com>
2025-12-10 02:09:53 +0100

    completed service and api endpoints for start simulation

--- server/src/main/java/LoadBalancer/Service/NodeReceiverService.java
+++ server/src/main/java/LoadBalancer/Service/NodeReceiverService.java
@@ -2,6 +2,8 @@ package LoadBalancer.Service;
 
 import LoadBalancer.Model.NodeReceiver;
 import LoadBalancer.Model.NodeWorker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import static LoadBalancer.Model.GlobalVariables.nodeReceiverList;
@@ -9,6 +11,7 @@ import static LoadBalancer.Model.GlobalVariables.nodeWorkerList;
 
 @Service
 public class NodeReceiverService implements Runnable {
+    private static final Logger logger = LoggerFactory.getLogger(NodeReceiverService.class);
     @Override
     public void run() {}
 
@@ -24,6 +27,21 @@ public class NodeReceiverService implements Runnable {
         for(int i = indexToRemove; i < nodeReceiverList.size(); i++){
             NodeReceiver nodeReceiver = nodeReceiverList.get(i);
             nodeReceiver.setNodeReceiverNumber(nodeReceiver.getNodeReceiverNumber() - 1);
+        }
+    }
+
+    public void startAllReceivers(){
+        for (NodeReceiver nr : nodeReceiverList){
+            new Thread(() -> runReceiver(nr)).start();
+        }
+    }
+
+    public void runReceiver(NodeReceiver nodeReceiver){
+        logger.info("Spawned receiver for Node " + nodeReceiver.getNodeReceiverNumber());
+        while(true){
+            //on sad ništa ne radi,
+            // on će biti zaslužan kasnije za
+            // preraspoređivanje poslova na druge receivere
         }
     }
 }
