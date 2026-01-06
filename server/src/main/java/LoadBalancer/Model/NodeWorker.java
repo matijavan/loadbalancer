d1897e24b328852d547e2f7b4343194247c01bca
Matija <matija.vandek@gmail.com>
2026-01-06 23:22:03 +0100

    1 line of code fix

--- server/src/main/java/LoadBalancer/Model/NodeWorker.java
+++ server/src/main/java/LoadBalancer/Model/NodeWorker.java
@@ -1,6 +1,5 @@
 package LoadBalancer.Model;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.concurrent.BlockingQueue;
@@ -10,7 +9,6 @@ import static LoadBalancer.Model.GlobalVariables.nodeReceiverList;
 @Data
 public class NodeWorker{
 
-    @NotNull
     private int NodeWorkerNumber;
     private Task taskCurrentlyBeingProcessed;
 
